9466b5f122615d546ca2650c8b26e38e7a18b749
ouajih_zakia <zakiaouajih12@gmail.com>
2023-02-11 23:02:40 +0100

    l

--- stage/resources/views/auth/passwords/email.blade.php
+++ stage/resources/views/auth/passwords/email.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header text-end">{{ __('msg.resetPwd') }}</div>
 
                 <div class="card-body">
                     @if (session('status'))
@@ -17,24 +17,24 @@
                     <form method="POST" action="{{ route('password.email') }}">
                         @csrf
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
+                        <div class="row mb-5 justify-content-center align-items-center">
+                            
+                            <div class="col-8  ">
+                                <input id="email" type="email" class="text-end form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                                
                                 @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
+                                <span class="invalid-feedback text-end" role="alert">
+                                    <strong>{{ $message }}</strong>
+                                </span>
                                 @enderror
                             </div>
+                            <label for="email" class="col-4 col-form-label text-end">{{ __('msg.email') }}</label>
                         </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                        <div class="row mb-0 ">
+                            <div class="col-md-6 offset-md-4 ">
                                 <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
+                                    {{ __('msg.resetLink') }}
                                 </button>
                             </div>
                         </div>
--- stage/resources/views/auth/passwords/reset.blade.php
+++ stage/resources/views/auth/passwords/reset.blade.php
@@ -3,9 +3,9 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-6">
             <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
+                <div class="card-header">{{ __('msg.lastName') }}</div>
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('password.update') }}">
